d1e5f3ab54c920befb2141fbaad5228dccee7562
Roberto Valdez <roberto.valdez.ponce@gmail.com>
2019-11-25 17:04:23 -0500

    Update azure_luis_comm.py

--- azure_luis_comm.py
+++ azure_luis_comm.py
@@ -10,7 +10,7 @@ import requests
 
 headers = {
     # Request headers
-    'Ocp-Apim-Subscription-Key': '55c4f6fd25a84e81bc2369845f18f9bf',
+    'Ocp-Apim-Subscription-Key': 'XXX',
 }
 
 params ={
@@ -25,7 +25,7 @@ params ={
 
 
 try:
-    r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/d8bd8e1e-7a79-4a00-aa99-4fbc8b1a8425',headers=headers, params=params)
+    r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/XXX',headers=headers, params=params)
     print(r.json())
 
 except Exception as e:
